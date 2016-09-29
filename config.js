45fe3311a07846c54f5a7947f7143816886444b5
Taylor Welling <tw3080@gmail.com>
2016-09-29 12:08:38 -0700

    added mLab url

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
+var mLabUrl = 'mongodb://user:password@ds027165.mlab.com:27165/mongo-shopping-list';
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
+    mLabUrl;
+    /*
     (process.env.NODE_ENV === 'production' ?
         'mongodb://localhost/shopping-list' :
         'mongodb://localhost/shopping-list-dev');
+        */
 exports.PORT = process.env.PORT || 8080;
